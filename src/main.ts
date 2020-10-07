d2b188480d74f00faaa22f805cd59848b08ad50c
Ali Faris <alihcompiler@gmail.com>
2020-10-07 23:36:28 +0300

    setup main.ts file

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import {Text} from "./Text";
+import {Password} from "./Password";
+import {Checkbox, Switch} from "./Checkbox";
+import {Radio} from "./Radio";
+import {NativeTime} from "./NativeTime";
+import {NativeDateTime} from "./NativeDateTime";
+import {NativeDate} from "./NativeDate";
+import {Slider} from "./Slider";
+import {Select} from "./Select";
+import {TextArray} from "./TextArray";
+import {AutoUpload} from "./AutoUpload";
+import {Localization} from "./Localization";
+import {MaterialRafDefaults} from "./MaterialRafDefaults";
+import {setupRafMaterial} from "./setup";
+import {KeyValueFormRenderer} from "./Renderers/FormRenderer/KeyValueFormRenderer/KeyValueFormRenderer";
+import {SimpleFormRenderer} from "./Renderers/FormRenderer/SimpleFormRenderer/SimpleFormRenderer";
+
+export {
+    Text,
+    Password,
+    Checkbox,
+    Switch,
+    Radio,
+    NativeTime,
+    NativeDateTime,
+    NativeDate,
+    Slider,
+    Select,
+    TextArray,
+    AutoUpload,
+    Localization,
+    MaterialRafDefaults,
+    setupRafMaterial,
+    KeyValueFormRenderer,
+    SimpleFormRenderer
+}
